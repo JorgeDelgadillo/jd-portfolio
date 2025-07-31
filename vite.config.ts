2a4f403019e52ae7c3a8a7f87b5fd2845290a522
Jorge Delgadillo <jorgdelgadillo@gmail.com>
2025-07-30 22:17:29 -0600

    update: fix allowed hosts

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,6 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: true,
+    allowedHosts: ['jorgedelgadillo.com', "portfolio-reacjsportfolio-pympdv-b19e2e-31-97-131-190.traefik.me"],
   },
 })
